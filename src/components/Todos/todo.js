46578cec9f84cadbbb2a1e9bb1bcd324e32a0ec1
Rahul Nanda <rahulnandakca@gmail.com>
2024-04-16 19:53:52 +0530

    added tick mark function

--- src/components/Todos/todo.js
+++ src/components/Todos/todo.js
@@ -4,32 +4,70 @@ import './todo.css'
 function Todo() {
   const [task, setTask] = useState('');
   const [todos, setTodos] = useState([]);
-  
+  const [completed, setCompleted] = useState(false);
+  const [completedTodos, setCompletedTodos] = useState([])
+  const handleAddTodo = () => {
+    if (task === '') {
+      alert('No todos entered')
+    } else {
+      setTodos([...todos, { id: Date.now(), task: task }])
+      setTask('')
+    }
+  }
+  const handleShowTodos = () => {
+    return (
+      <ul>
+        {todos.map((item, index) => {
+          return (
+            <li className='p-1' key={index}>{item.task} <i onClick={() => {
+              setCompletedTodos([...completedTodos, item])
+              setTodos(todos.filter(value => value.id !== item.id))
+            }} className="fa-solid fa-check ms-auto me-3"></i>
+              <i onClick={() => {
+                setTodos(todos.filter(value => value.id !== item.id))
+              }} className="fa-solid fa-trash"></i></li>
+          )
+        })
+        }
+      </ul>
+    )
+  }
+  const showCompletedTodos = () => {
+    return (
+      <ul>
+        {completedTodos.map((index, item) => {
+          return (
+            setCompleted(true),
+            <li key={index}>{item.task} <i onClick={() => {
+              setCompletedTodos(completedTodos.filter(value => value.id !== item.id))
+            }} class="fa-solid fa-trash"></i></li>
+          )
+        })
+        }
+        <li></li>
+      </ul>
+    )
+  }
+
   return (
     <div className='todo-container col-md-6'>
       <h2 className='heading mb-4'>Todo Lists</h2>
       <div className="input-area mb-4">
         <input className='p-1' type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder='Enter todos' />
-        <button className='add-btn p-1' onClick={() => {
-          setTodos([...todos, {id: Date.now(), task:task}])
-          setTask('')
-        }
-      }>Add</button>
+        <button className='add-btn p-1' onClick={() => handleAddTodo()}>Add</button>
       </div>
       <section className='show-todos'>
-        <ul>
-          {todos.map((item,index) => {
-            return(
-              <li key={index}>{item.task} <i onClick={() => {
-                setTodos(todos.filter(value => value.id !== item.id))
-              }} className="fa-solid fa-trash"></i></li>
-            )
-          })
-        }
-        </ul>
+        <div className="todo-btns">
+          <button className='show-todo-btn' onClick={() => handleShowTodos()}>Active Todos</button>
+          <button className='show-todo-btn' onClick={() => showCompletedTodos()}>Completed Todos</button>
+        </div>
+        <div className="display-todos">
+          {completed ? showCompletedTodos() : handleShowTodos()}
+          {/* {handleShowTodos()} */}
+        </div>
       </section>
-    </div>
+    </div >
   )
 }
 
-export default Todo
+export default Todo;
